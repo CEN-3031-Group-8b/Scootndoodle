55fb739a812177b6433ace583a9ac23eb6c4f905
seanconnolly2345 <32102413+seanconnolly2345@users.noreply.github.com>
2019-12-09 13:28:00 -0500

    Update Orders.js

--- client/src/views/Admin/Orders.js
+++ client/src/views/Admin/Orders.js
@@ -4,7 +4,7 @@ import {Row, Button} from 'react-bootstrap'
 import './orders.css';
 import Navigation from '../../components/Navigation/Navigation';
 
-
+// imports the tableviewer component created and displays it on this page with the extension 'adminportal'
 export const Orders = () => (
     <React.Fragment>     
         <body className = "tableView">
